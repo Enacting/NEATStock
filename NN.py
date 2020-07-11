0184335fb0dd4c91f9f1fdc01af1506cb8b764ce
adityakhannaadk <38283382+adityakhannaadk@users.noreply.github.com>
2020-07-11 11:41:01 +0100

    Add files via upload

---
+++ NN.py
@@ -0,0 +1,27 @@
+#The defaulty neural network with defaulty parameters to be evolved using NEAT. 
+import numpy as np
+
+
+initial_money = 10000
+window_size = 30
+skip = 1
+
+class neuralnetwork:
+    def __init__(self, id_, hidden_size = 128):
+        self.W1 = np.random.randn(window_size, hidden_size) / np.sqrt(window_size)
+        self.W2 = np.random.randn(hidden_size, 3) / np.sqrt(hidden_size)
+        self.fitness = 0
+        self.id = id_
+
+def relu(X):
+    return np.maximum(X, 0)
+    
+def softmax(X):
+    e_x = np.exp(X - np.max(X, axis=-1, keepdims=True))
+    return e_x / np.sum(e_x, axis=-1, keepdims=True)
+
+def feed_forward(X, nets):
+    a1 = np.dot(X, nets.W1)
+    z1 = relu(a1)
+    a2 = np.dot(z1, nets.W2)
+    return softmax(a2)
